745911f769d9e9e3a3c1102087e9011258836f7b
JustinWuAstro <33946706+JustinWuAstro@users.noreply.github.com>
2023-02-28 21:09:37 +0800

    edit something

---
+++ src/component/Main.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul>
+            <li>A</li>
+            <li>B</li>
+            <li>C</li>
+        </ul>
+        </main>
+        
+    )
+}
